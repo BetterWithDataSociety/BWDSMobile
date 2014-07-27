56b1857806b0863db9b4aaa7a7b4b0df9a422510
Ian Ibbotson <ian.ibbotson@k-int.com>
2014-07-27 23:22:01 +0100

    Working on logging

--- MobileCitizen/www/js/index.js
+++ MobileCitizen/www/js/index.js
@@ -33,17 +33,47 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-        app.receivedEvent('deviceready');
+        // app.receivedEvent('deviceready');
+        window.plugin.backgroundMode.enable();
+        // Watch location, calling onWatchCurrentPositionSuccess or onWatchCurrentPositionError accordingly
+	navigator.geolocation.watchCurrentPosition(app.onWatchCurrentPositionSuccess, app.onWatchCurrentPositionError);
+        // Queue up an event to update position every 5000s
+	setTimeout(app.updatePosition , 5000);
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
-    }
+        // var parentElement = document.getElementById(id);
+        // var listeningElement = parentElement.querySelector('.listening');
+        // var receivedElement = parentElement.querySelector('.received');
+        // listeningElement.setAttribute('style', 'display:none;');
+        // receivedElement.setAttribute('style', 'display:block;');
+        // console.log('Received Event: ' + id);
+    },
+    onWatchCurrentPositionError: function() {
+    },
+    onWatchCurrentPositionSuccess: function(position) {
+	app.position = position;
+	//<-- don't do anything intensive here , especially HTML updates, your display may hang with too much to do -->
+    },
+    updatePosition: function () {
+	//<!-- update your HTML here , check for valid lat,lon (i.e. they should be different than -999) -->
+	//<!-- call this function again -->
+        ctr++
+        console.log('Processing '+app.position+' '+ctr);
+        // app.locationQueue.push(app.position);
+        app.lastFive.push(app.position);
+        if(app.lastFive.length > 5) {
+          app.lastFive.splice(5, 1);
+        } 
+        var lastLocElement = document.getElementById('lastLoc');
+        lastLoc.empty();
+        for(e : app.lastFive) {
+          lastLoc.append('<tr><td>'+e.timestamp+'</td><td>'+e.position.location+'</td></tr>');
+        }
+	setTimeout(app.updatePosition , 5000);
+    },
+    position:-1,
+    locationQueue:[],
+    lastFive:[],
+    ctr:0
 };
